68854e55816ef1f00c178adad96efa4b1ede13f4
Matthew Canabarro <mec357@cornell.edu>
2023-09-28 10:19:19 -0400

    added helper text

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -26,13 +26,15 @@ const SectionHeader = ({ use, btnFunc, showNum }) => {
     h1: {
       color: (theme) => theme.palette.text.primary
     },
-    h2: {
+    body2: {
+      opacity: 0.8,
       color: (theme) => theme.palette.text.secondary
     },
     text: {
       display: "flex",
       gap: 12,
       alignItems: "baseline"
+      // alignItems: "center"
     }
   }
   const { numItems, numPeople, isActiveItem } = useAppContext();
@@ -47,6 +49,11 @@ const SectionHeader = ({ use, btnFunc, showNum }) => {
       <div style={styles.text}>
         <Typography variant="h1" sx={styles.h1}>{use}{showNum && `: ${chooseNum(use)}`}</Typography>
         {/* {showNum && <Typography variant="h2" sx={styles.h2}>Scroll here to see all {use.toLowerCase()}</Typography>} */}
+        {numItems > 0 && !isActiveItem && use === "Items" &&
+          <Typography variant="body2" sx={styles.body2}>Select one to assign</Typography>}
+        {isActiveItem && use === "People" &&
+          <Typography variant="body2" sx={styles.body2}>Select who got this</Typography>}
+
       </div>
       {btnFunc &&
         <Button
